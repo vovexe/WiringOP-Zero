c043219bdc5b5b35ae624f0aae0f6f0e4acff786
vovexe <vovexe@gmail.com>
2017-07-14 15:32:50 +0200

    Update buzzer.c

--- buzzer.c
+++ buzzer.c
@@ -15,9 +15,18 @@ int main( int argc, char *argv[] ) {
 		exit(1);
     pinMode(DHT_PIN, OUTPUT);
 	
-	int iTone = 750;
 	int iNumBeeps = 10;
+	if (argc >= 2) {
+		iNumBeeps = strtol(argv[1], NULL, 10);
+	}
 	double dBeepLenghtInSeconds = 0.25;
+	if (argc >= 3) {
+		dBeepLenghtInSeconds = atof(argv[2]);
+	}
+	int iTone = 750;
+	if (argc >= 4) {
+		iTone = strtol(argv[3], NULL, 10);
+	}
 	int iLenghtLimiterIndex = dBeepLenghtInSeconds * 1000000.0 / ( (double)(2 * iTone) );
 	for (int j = 0; j < iNumBeeps; j++) {
 		for (int i = 0; i < iLenghtLimiterIndex; i++) {
@@ -26,7 +35,7 @@ int main( int argc, char *argv[] ) {
 			digitalWrite(DHT_PIN, LOW);
 			delayMicroseconds(iTone);
 		}
-		delayMicroseconds(dBeepLenghtInSeconds * 1000000);
+		if (j < iNumBeeps-1) delayMicroseconds(dBeepLenghtInSeconds * 1000000);
 	}
 	digitalWrite(DHT_PIN, LOW);
 	
